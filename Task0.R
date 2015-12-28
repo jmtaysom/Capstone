b98bff0ac9f8d430158e64336dac449c34981742
jmtaysom <james.taysom@gmail.com>
2015-12-28 08:34:34 -0500

    gave up on collocations

--- Task0.R
+++ Task0.R
@@ -14,10 +14,19 @@ rm(url)
 rm(zip)
 # Create a connection to the text file and read in a selection.
 con <- file('final/en_US/en_US.blogs.txt',"r")
-blogs <- readLines(con, 10000)
+twi.con <- file('final/en_US/en_US.twitter.txt','r')
+news.con <-file('final/en_US/en_US.news.txt','r') 
+blogs <- readLines(con, 50000)
+twitter <- readLines(twi.con, 50000)
+news <- readLines(news.con, 50000)
 #blog.mid <- readLines(con, 20000)
 close(con)
 rm(con)
+close(twi.con)
+rm(twi.con)
+close(news.con)
+rm(news.con)
+
 #write.table(blogs, file='blogs_sample.txt')
 blogs <- readLines(file('blogs_sample.txt'))
 #blogs <- iconv(blogs, to = "utf-8", sub="")
@@ -35,13 +44,19 @@ rm(blogs)
 #tdm <- TermDocumentMatrix(corpus, control = list(tokenize = BigramTokenizer))
 # ngram <- NGramTokenizer(blogs)
 
-#blogs.ngram <- dfm(blogs, ngrams = 2:2, verbose = FALSE)
-
+blog.ngram <- dfm(blogs, ngrams = 3:3, verbose = FALSE)
+twit.ngram <- dfm(twitter, ngrams = 3:3, verbose = FALSE)
+news.ngram <- dfm(news, ngrams = 3:3, verbose = FALSE)
 #bigram <- tokenize(toLower(blogs), removePunct = TRUE, removeNumbers = TRUE, ngrams =2)
-bigram.test<- sum(ntoken(bigram))
-topfeatures(blogs.ngram, n = ncol(blogs.ngram)) -> tf
-data.frame(tf) -> df.tf
-df.tf <- add_rownames(df.tf)
+#bigram.test<- sum(ntoken(bigram))
+tf.blog <- topfeatures(blog.ngram, n = ncol(blog.ngram))
+tf.blog <- data.frame(tf.blog)
+tf.blog <- add_rownames(tf.blog)
+tf.twit <- topfeatures(twit.ngram, n = ncol(twit.ngram))
+tf.twit <- data.frame(tf.twit)
+tf.twit <- add_rownames(tf.twit)
+
+
 temp<- str_split_fixed(df.tf$rowname,'_',2)
 df.tf <- cbind(df.tf,temp)
 dup <- duplicated(df.tf$`1`)
@@ -54,12 +69,36 @@ as.character(df.tf.sub[df.tf.sub$`1` == 'merry',]$`2`)
 #tf <- textfile('blogs_sample.txt', textField = 'text')
 
 # colocations are amazing
-coloc3 <- collocations(blogs, size=3)
-coloc3 <- coloc3[order(-coloc3$count)]
-coloc2<- collocations(blogs, size=2)
-coloc2 <- coloc2[order(-coloc2$count)]
-#coloc[coloc$word1 == 'there',]$word2[1]
-#coloc[coloc$word1 == 'what' & coloc$word2 == 'the',]$word3[2]
+coloc3.blog <- collocations(blogs, size=3)
+coloc3.twit <- collocations(twitter, size=3)
+coloc3.news <- collocations(news, size=3)  
+coloc3.comb <- rbind(coloc3.blog, coloc3.news, coloc3.twit)
+coloc3.comb <- aggregate(count~word1+word2+word3, data=coloc3.comb, FUN=sum)
+coloc3.comb <- coloc3.comb[order(-coloc3.comb$count),]
+coloc3.comb <- coloc3.comb[coloc3.comb$count >1,]
+rm(coloc3.news)
+rm(coloc3.blog)
+rm(coloc3.twit)
+
+coloc2.blog <- collocations(blogs, size=2)
+coloc2.twit <- collocations(twitter, size=2)
+coloc2.news <- collocations(news, size=2)  
+coloc2.comb <- rbind(coloc2.blog, coloc2.news, coloc2.twit)
+coloc2.comb <- aggregate(count~word1+word2+word3, data=coloc2.comb, FUN=sum)
+coloc2.comb <- coloc2.comb[order(-coloc2.comb$count),]
+coloc2.comb <- coloc2.comb[coloc2.comb$count >1,]
+rm(coloc2.news)
+rm(coloc2.blog)
+rm(coloc2.twit)
+
+
+
+coloc2.comb[coloc2.comb$word1 == 'there',]$word2[1]
+coloc3.comb[coloc3.comb$word1 == 'case' & coloc3.comb$word2 == 'of',]$word3[2]
+
+rm(blogs)
+rm(news)
+rm(twitter)
 #maybe create tables using tokenize of unigrams, bigrams and trigrams
 
 c3<- cbind(coloc3$word1, coloc3$word2)
